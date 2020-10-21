ae9ed15eedbdc407c885a13d90fe3e4c994439b5
JYbmarawcp <fjh2333@gmail.com>
2020-10-21 20:04:44 +0800

    初始化serve并解决跨域请求

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+  
+})
+
+server.listen(3001, '0.0.0.0', () => console.log('正在监听 3001 端口'))
---
+++ src/components/simple.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="simpel-upload-container">
+    <div class="total-progress">
+      <div class="btns">
+        <el-button-group>
+          <el-button type="primary" icon="el-icon-edit"></el-button>
+          <el-button type="primary" icon="el-icon-share"></el-button>
+          <el-button type="primary" icon="el-icon-delete"></el-button>
+        </el-button-group>
+      </div>
+    </div>
+
+    <div class="file-list">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
