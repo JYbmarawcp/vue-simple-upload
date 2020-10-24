b019f8b16e24319b35b53b60bc40441e710b09e2
JYbmarawcp <fjh2333@gmail.com>
2020-10-24 23:51:17 +0800

    nothing

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const server = http.createServer()
 server.on('request', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
-  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-  
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
 })
 
 server.listen(3001, '0.0.0.0', () => console.log('正在监听 3001 端口'))
