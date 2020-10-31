316345eef6b5663e9e774f40e13e795a4a6f0afb
JYbmarawcp <fjh2333@gmail.com>
2020-10-31 21:40:39 +0800

    重新提交

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="master-upload-container">
+      <Simple
+        ref="upload"
+        :accept="accepts"
+        :base-url="baseUrl"
+        :chunk-size="chunkSize"
+      >
+        <div slot="tip" class="upload-tip">
+          <i class="el-icon-info"></i>:
+          只能上传: zip,rar
+        </div>
+      </Simple>
+    </div>
+  </div>
+</template>
+
+<script>
+import Simple from "./components/simple"
+
+export default {
+  name: 'app',
+  components: {
+    Simple
+  },
+  data () {
+    return {
+      accepts: '.jpg, .jpeg, .png',
+      chunkSize: 50 * 1024 *1024,
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    baseUrl() {
+      return 'http://localhost:3003/';
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
